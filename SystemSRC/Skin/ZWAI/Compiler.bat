ececc669e5f90e4e3cc8ec0320f2134c225ea9d8
recaptcha-UX <57635649+recaptcha-UX@users.noreply.github.com>
2019-11-12 16:26:36 -0500

    Add files via upload

---
+++ SystemSRC/Skin/ZWAI/Compiler.bat
@@ -0,0 +1,18 @@
+@echo off
+echo ......................................
+echo .Looking Glass compiler              .
+echo . For Looking Glass 5                .
+echo ......................................
+
+pause
+
+echo Looking-Glass will now be compiled For Ms2.1 Child Zwai
+
+pause
+
+Core.exe Looking_Glass_5.skn images
+
+echo Done!
+
+pause
+
